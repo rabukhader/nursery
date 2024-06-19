96179c6db0039327b09021a73b4ea9cd6db8bab1
RasheedAbuKhader <rasheed@quizplus.com>
2024-06-19 23:35:48 +0300

    stream-ok

--- lib/ui/home/monitoring_page/monitoring_page.dart
+++ lib/ui/home/monitoring_page/monitoring_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 import 'package:get_it/get_it.dart';
 import 'package:nursery/services/firestore_service.dart';
 import 'package:nursery/ui/home/book_room_parent/book_room_provider.dart';
@@ -6,7 +7,6 @@ import 'package:nursery/ui/home/monitoring_page/monitoring_provider.dart';
 import 'package:nursery/ui/home/widgets/loader.dart';
 import 'package:nursery/utils/colors.dart';
 import 'package:provider/provider.dart';
-import 'package:video_player/video_player.dart';
 
 class MonitoringPage extends StatefulWidget {
   final BookingRoom bookedRoomData;
@@ -17,22 +17,29 @@ class MonitoringPage extends StatefulWidget {
 }
 
 class _MonitoringPageState extends State<MonitoringPage> {
-  late VideoPlayerController _controller;
+  late VlcPlayerController _vlcViewController;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(
-      Uri.parse('rtmp://192.168.1.217:8080/hls/test.m3u8'),
-    )..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
+    _vlcViewController = VlcPlayerController.network(
+      'rtmp://192.168.1.217/live/test', // Replace with your RTMP URL
+      autoPlay: true,
+      options: VlcPlayerOptions(
+        advanced: VlcAdvancedOptions([
+          VlcAdvancedOptions.networkCaching(3000), // 3 seconds caching
+          VlcAdvancedOptions.clockJitter(300), // Adjust as necessary
+        ]),
+        rtp: VlcRtpOptions([
+          VlcRtpOptions.rtpOverRtsp(true), // Force using TCP
+        ]),
+      ),
+    );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _vlcViewController.dispose();
     super.dispose();
   }
 
@@ -54,14 +61,13 @@ class _MonitoringPageState extends State<MonitoringPage> {
                   centerTitle: true,
                 ),
                 body: Center(
-                  child: _controller.value.isInitialized
-                      ? AspectRatio(
-                          aspectRatio: _controller.value.aspectRatio,
-                          child: VideoPlayer(_controller),
-                        )
-                      : CircularProgressIndicator(),
-                ),
-              );
+                  child: VlcPlayer(
+                    controller: _vlcViewController,
+                    aspectRatio: 16 / 9,
+                    placeholder:
+                        const Center(child: CircularProgressIndicator()),
+                  ),
+                ));
       },
     );
   }
